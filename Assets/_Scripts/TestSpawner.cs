99bd71c82509b9dae2e85f4e7c5c892ed2a0bbcf
BRaj <raj.baldev001@gmail.com>
2023-12-01 10:46:01 -0500

    Update TestSpawner.cs
    updated

--- Assets/_Scripts/TestSpawner.cs
+++ Assets/_Scripts/TestSpawner.cs
@@ -25,7 +25,6 @@ public class TestSpawner : MonoBehaviour
         }
         createdPrefabs2.Clear();
     }
-
     public void SpawnPrefab()
     {
         var position = Random.insideUnitSphere * 5;
@@ -33,37 +32,26 @@ public class TestSpawner : MonoBehaviour
         
         //createdPrefabs.Add(Instantiate(prefab, transform.position, Quaternion.identity));
     }
-    
     public void SpawnPrefab2()
     {
         var position = Random.insideUnitSphere * 5;
         createdPrefabs2.Add(Instantiate(prefab2, position, Quaternion.identity));
         //createdPrefabs.Add(Instantiate(prefab, transform.position, Quaternion.identity));
     }
-    
-    
     public void SaveGame()
     {
         SaveData data = new SaveData();
-        SaveData data2 = new SaveData();
         foreach (var item in createdPrefabs)
         {
-            data.Add(item.transform.position);
+            data.Add(item);
         }
-        var dataToSave1 = JsonUtility.ToJson(data);
-        //saveSystem.SaveData(dataToSave);
-
-        //SaveData2 data2 = new SaveData2();
         foreach (var item in createdPrefabs2)
         {
-            data2.Add(item.transform.position);
+            data.Add(item);
         }
-        var dataToSave2 = JsonUtility.ToJson(data2);
-        var dataToSave = (dataToSave1+dataToSave2);
+        var dataToSave = JsonUtility.ToJson(data);
         saveSystem.SaveData(dataToSave);
-        
     }
-
     public void LoadGame()
     {
         Clear();
@@ -73,65 +61,42 @@ public class TestSpawner : MonoBehaviour
         {
             SaveData data = JsonUtility.FromJson<SaveData>(dataToLoad);
             //Debug.Log(data);
-            foreach (var positionData in data.positionData)
+            foreach (var objData in data.objectData)
             {
-                createdPrefabs.Add(Instantiate(prefab, positionData.GetValue(), Quaternion.identity));
+                GameObject newObj = new GameObject();
+                if (objData.type.StartsWith("Cube"))
+                    newObj = Instantiate(prefab, objData.position.GetValue(), Quaternion.identity);
+                if (objData.type.StartsWith("Sphere"))
+                    newObj = Instantiate(prefab2, objData.position.GetValue(), Quaternion.identity);
+                newObj.name = objData.type;
+                createdPrefabs.Add(newObj);
             }
         }
     } 
     [Serializable]
     public class SaveData
     {
-        public List<Vector3Serialzation> positionData;
-        public List<Vector3Serialzation> positionData2;
+        public List<OjbectSerialization> objectData;
         public SaveData()
         {
-            //Clear();
-            positionData = new List<Vector3Serialzation>();
-            //positionData2 = new List<Vector3Serialzation>();
+            objectData = new List<OjbectSerialization>();
         }
-        public void Add(Vector3 position)
+        public void Add(GameObject obj)
         {
-            //Clear();
-            positionData.Add(new Vector3Serialzation(position));
-            //positionData2.Add(new Vector3Serialzation(position));
+            objectData.Add(new OjbectSerialization(obj.name, obj.transform.position));
         }
-        /*
-        public SaveData2()
-        {
-            //Clear();
-            //positionData = new List<Vector3Serialzation>();
-            positionData2 = new List<Vector3Serialzation>();
-        }
-
-        public void Add2(Vector3 position)
-        {
-            //Clear();
-            //positionData.Add(new Vector3Serialzation(position));
-            positionData2.Add(new Vector3Serialzation(position));
-        }*/
-
-      
-        
-        
     }
-    /*
-
     [Serializable]
-    public class SaveData2
+    public class OjbectSerialization
     {
-        public List<Vector3Serialzation> positionData2;
-        public SaveData2()
+        public Vector3Serialzation position;
+        public string type;
+        public OjbectSerialization(string t, Vector3 pos)
         {
-            positionData2 = new List<Vector3Serialzation>();
+            this.type = t;
+            this.position = new Vector3Serialzation(pos);
         }
-        public void Add2(Vector3 position)
-        {
-            positionData2.Add2(new Vector3Serialzation(position));
-        }
-    }*/
-
-   
+    }
     [Serializable]
     public class Vector3Serialzation
     {
